39078bebd8a592933bd87d1017db821e6a9b4d84
tmdqhs <tmdqh>
2023-09-04 22:41:58 +0900

    feat: 로그인UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import FirstPage from "./components/pages/First/FirtstPage";
-<!-- import LoginPage from "./components/pages/Login/LoginPage"; -->
+import LoginPage from "./components/pages/Login/LoginPage";
 import MapPage from './components/pages/Map/MapPage'
 import TimelinePage from './components/pages/Timeline/TimelinePage'
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import UploadComponent from "./components/pages/upload/UploadComponent";
 import axios from 'axios';
-
+import "./App.css"
+import Footer from "./components/pages/common/Footer";
 // axios.defaults.baseURL = process.env.REACT_APP_URL;
 axios.defaults.baseURL = 'http://localhost:5000';
-// axios.defaults.withCredentials = true;
+axios.defaults.withCredentials = true;
 
 function App() {
   return (
@@ -16,8 +17,8 @@ function App() {
       <Routes>
         {/* 첫 페이지 */}
         <Route path="/" element={<FirstPage />} />
-<!--         {/* 로그인 페이지 */}
-        <Route path="/login" element={<LoginPage />} /> -->
+       {/* 로그인 페이지 */}
+        <Route path="/login" element={<LoginPage />} /> 
         {/* 업로드 페이지 */}
         <Route path="/upload" element={<UploadComponent />} />
         {/* 지도 페이지 */}
@@ -25,6 +26,7 @@ function App() {
         {/* 타임라인 페이지 */}
         <Route path='/timeline' element={<TimelinePage />} />
       </Routes>
+        <Footer />
       
     </BrowserRouter>
   );
--- src/components/pages/First/FirtstPage.tsx
+++ src/components/pages/First/FirtstPage.tsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import BoxComponent from "./List";
 import { dummyData } from "./data";
+import axios from "axios";
 
 const App: React.FC = () => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null as any);
+  console.log(data)
+  useEffect( () => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.post('http://localhost:5000/main');
+        
+        setData(response.data);
+      } catch (error) {
+        setError(error);
+      }
+    };
+
+    fetchData()
+  }, [])
+
   return (
     <div>
       {dummyData.map((item, index) => (
