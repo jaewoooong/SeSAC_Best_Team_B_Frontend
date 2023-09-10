a20c3f5d0fc4ac822ccb4774e66261f3f077e2fe
DESKTOP-1343178\96tmd <96tmdqh@naver.com>
2023-09-10 16:50:44 +0900

    feat: 메인페이지 기본 UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import MainPage from "./components/pages/First/MainPage";
 import LoginPage from "./components/pages/Login/LoginPage";
-import MapPage from './components/pages/Map/MapPage'
-import TimelinePage from './components/pages/Timeline/TimelinePage'
+import Main from "./components/pages/main/Main";
+import MapPage from "./components/pages/Map/MapPage";
+import TimelinePage from "./components/pages/Timeline/TimelinePage";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import UploadComponent from "./components/pages/upload/UploadComponent";
-import axios from 'axios';
-import "./App.css"
+import axios from "axios";
+import "./App.css";
 import Footer from "./components/pages/common/Footer";
 import { useState } from "react";
 // axios.defaults.baseURL = 'http://localhost:5000';
@@ -19,15 +20,17 @@ function App() {
       <Routes>
         {/* 첫 페이지 */}
         <Route path="/" element={<MainPage />} />
+        {/* 첫 페이지 */}
+        <Route path="/main" element={<Main />} />
         {/* 로그인 페이지 */}
         <Route path="/login" element={<LoginPage />} />
         {/* 지도 페이지 */}
-        <Route path='/map' element={<MapPage />} />
+        <Route path="/map" element={<MapPage />} />
         {/* 타임라인 페이지 */}
-        <Route path='/timeline' element={<TimelinePage />} />
+        <Route path="/timeline" element={<TimelinePage />} />
       </Routes>
-      <Footer setUploadState={setUploadState} />
-      {uploadState && <UploadComponent />}
+      {/* <Footer setUploadState={setUploadState} />
+      {uploadState && <UploadComponent />} */}
     </BrowserRouter>
   );
 }
---
+++ src/components/pages/main/Main.tsx
@@ -0,0 +1,330 @@
+import React from "react";
+import "./main.css";
+import { Link } from "react-router-dom";
+interface Props {
+  message: string;
+}
+
+const Main: React.FC = () => {
+  return (
+    <div className="background">
+      <div className="Pagelayout">
+        <aside>
+          <Link to="/">
+            <img className="menu" src="./Home.png" alt="" />
+          </Link>
+          <Link to="/map">
+            <img className="menu" src="./earth.png" alt="" />
+            <span>settings</span>
+          </Link>
+          <Link to="/timeline">
+            <img className="menu" src="./Image.png" alt="" />
+          </Link>
+          <Link to="/login">
+            <img className="menu" src="./Settings.png" alt="" />
+          </Link>
+        </aside>
+
+        <main className="mainPage">
+          <div className="middle">
+            <header>
+              <h2>BigBang</h2>
+              <div className="search-container">
+                <input
+                  type="text"
+                  className="search-input"
+                  placeholder="search"
+                />
+                <img src="./Earth.png" alt="Search" className="search-icon" />
+              </div>
+            </header>
+            <button>룸만들기</button>
+            <ul className="roomList">
+              <li>
+                <div>
+                  <img src="./girl1-removebg-preview.png" alt="" />
+                  <span>1</span>
+                </div>
+                {/* <h3>추억이 함께</h3> */}
+                <div>
+                  <h2>Micle</h2>
+                  <h3>family</h3>
+                  <h4>zhugek asokjdoa</h4>
+                </div>
+                <div>
+                  <span>슬로건</span>
+                </div>
+              </li>
+              <li>
+                <div>
+                  <img src="./girl1-removebg-preview.png" alt="" />
+                  <span>1</span>
+                </div>
+                {/* <h3>추억이 함께</h3> */}
+                <div>
+                  <h2>Micle</h2>
+                  <h3>family</h3>
+                  <h4>zhugek asokjdoa</h4>
+                </div>
+                <div>
+                  <span>슬로건</span>
+                </div>
+              </li>
+              <li>
+                <div>
+                  <img src="./girl1-removebg-preview.png" alt="" />
+                  <span>1</span>
+                </div>
+                {/* <h3>추억이 함께</h3> */}
+                <div>
+                  <h2>Micle</h2>
+                  <h3>family</h3>
+                  <h4>zhugek asokjdoa</h4>
+                </div>
+                <div>
+                  <span>슬로건</span>
+                </div>
+              </li>
+              <li>
+                <div>
+                  <img src="./girl1-removebg-preview.png" alt="" />
+                  <span>1</span>
+                </div>
+                {/* <h3>추억이 함께</h3> */}
+                <div>
+                  <h2>Micle</h2>
+                  <h3>family</h3>
+                  <h4>zhugek asokjdoa</h4>
+                </div>
+                <div>
+                  <span>슬로건</span>
+                </div>
+              </li>
+              <li>
+                <div>
+                  <img src="./girl1-removebg-preview.png" alt="" />
+                  <span>1</span>
+                </div>
+                {/* <h3>추억이 함께</h3> */}
+                <div>
+                  <h2>Micle</h2>
+                  <h3>family</h3>
+                  <h4>zhugek asokjdoa</h4>
+                </div>
+                <div>
+                  <span>슬로건</span>
+                </div>
+              </li>
+              <li>
+                <div>
+                  <img src="./girl1-removebg-preview.png" alt="" />
+                  <span>1</span>
+                </div>
+                {/* <h3>추억이 함께</h3> */}
+                <div>
+                  <h2>Micle</h2>
+                  <h3>family</h3>
+                  <h4>zhugek asokjdoa</h4>
+                </div>
+                <div>
+                  <span>슬로건</span>
+                </div>
+              </li>
+              <li>
+                <div>
+                  <img src="./girl1-removebg-preview.png" alt="" />
+                  <span>1</span>
+                </div>
+                {/* <h3>추억이 함께</h3> */}
+                <div>
+                  <h2>Micle</h2>
+                  <h3>family</h3>
+                  <h4>zhugek asokjdoa</h4>
+                </div>
+                <div>
+                  <span>슬로건</span>
+                </div>
+              </li>
+              <li>
+                <div>
+                  <img src="./girl1-removebg-preview.png" alt="" />
+                  <span>1</span>
+                </div>
+                {/* <h3>추억이 함께</h3> */}
+                <div>
+                  <h2>Micle</h2>
+                  <h3>family</h3>
+                  <h4>zhugek asokjdoa</h4>
+                </div>
+                <div>
+                  <span>슬로건</span>
+                </div>
+              </li>
+              <li>
+                <div>
+                  <img src="./girl1-removebg-preview.png" alt="" />
+                  <span>1</span>
+                </div>
+                {/* <h3>추억이 함께</h3> */}
+                <div>
+                  <h2>Micle</h2>
+                  <h3>family</h3>
+                  <h4>zhugek asokjdoa</h4>
+                </div>
+                <div>
+                  <span>슬로건</span>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <div className="rightside">
+            <div>
+              <div>
+                <h3>Memory Canvases</h3>
+              </div>
+              <div className="graph_Rank">
+                <div className="rank2">
+                  <svg
+                    className="star-icon"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      className="star-path"
+                      d="M12 2l3.09 6.26L22 9.27l-5 4.87L18.18 22 12 18.27 5.82 22 7 14.14l-5-4.87 6.91-1.01L12 2z"
+                    />
+                    <text
+                      x="11.5"
+                      y="14"
+                      alignment-baseline="middle"
+                      text-anchor="middle"
+                      fill="#fff"
+                      font-size="8"
+                    >
+                      2
+                    </text>
+                  </svg>
+                  <div>
+                    <img
+                      width={40}
+                      height={40}
+                      src="././girl1-removebg-preview.png"
+                      alt=""
+                    />
+                    <h3>namesdsdssd</h3>
+                  </div>
+                </div>
+                <div className="rank1">
+                  <svg
+                    className="star-icon"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      className="star-path"
+                      d="M12 2l3.09 6.26L22 9.27l-5 4.87L18.18 22 12 18.27 5.82 22 7 14.14l-5-4.87 6.91-1.01L12 2z"
+                    />
+                    <text
+                      x="11.5"
+                      y="14"
+                      alignment-baseline="middle"
+                      text-anchor="middle"
+                      fill="#fff"
+                      font-size="8"
+                    >
+                      1
+                    </text>
+                  </svg>
+                  <div>
+                    <img
+                      width={40}
+                      height={40}
+                      src="././girl1-removebg-preview.png"
+                      alt=""
+                    />
+                    <h3>namesdsdssd</h3>
+                  </div>
+                </div>
+                <div className="rank3">
+                  <svg
+                    className="star-icon"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      className="star-path"
+                      d="M12 2l3.09 6.26L22 9.27l-5 4.87L18.18 22 12 18.27 5.82 22 7 14.14l-5-4.87 6.91-1.01L12 2z"
+                    />
+                    <text
+                      x="11.5"
+                      y="14"
+                      alignment-baseline="middle"
+                      text-anchor="middle"
+                      fill="#fff"
+                      font-size="8"
+                    >
+                      3
+                    </text>
+                  </svg>
+                  <div>
+                    <img
+                      width={40}
+                      height={40}
+                      src="././girl1-removebg-preview.png"
+                      alt=""
+                    />
+                    <h3>assd</h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="rank4_10">
+                <div>
+                  <svg
+                    className="star-icon"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="40"
+                    height="40"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      className="star-path"
+                      d="M12 2l3.09 6.26L22 9.27l-5 4.87L18.18 22 12 18.27 5.82 22 7 14.14l-5-4.87 6.91-1.01L12 2z"
+                    />
+                  </svg>
+                  <div>micle</div>
+                  <div>75%</div>
+                </div>
+                <div>
+                  <svg
+                    className="star-icon"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="40"
+                    height="40"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      className="star-path"
+                      d="M12 2l3.09 6.26L22 9.27l-5 4.87L18.18 22 12 18.27 5.82 22 7 14.14l-5-4.87 6.91-1.01L12 2z"
+                    />
+                  </svg>
+                  <div>micle</div>
+                  <div>75%</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
