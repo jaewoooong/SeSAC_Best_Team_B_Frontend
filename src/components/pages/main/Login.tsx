1a3331656db766b2aa8efed2171a3d74e89122b0
Jeon-Min-seok <142853339+Jeon-Min-seok@users.noreply.github.com>
2023-09-19 03:18:52 +0900

    Update Login.tsx

--- src/components/pages/main/Login.tsx
+++ src/components/pages/main/Login.tsx
@@ -37,7 +37,7 @@ const LoginPage: React.FC = () => {
             <div>google Login</div>
           </a>
         </div>
-        <img src="./loginBG.png" alt="" />
+        <img src="./loginBg.png" alt="" />
       </div>
     </div>
   );
